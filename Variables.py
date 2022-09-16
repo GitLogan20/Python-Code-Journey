ee058bd6fcf2413376bce9e65f6b02cc9b47f272
antaeuslogan <antaeuslogan20@yahoo.com>
2022-09-15 22:45:40 -0400

    Python Personal Project

---
+++ Variables.py
@@ -0,0 +1,18 @@
+#Mixing data types
+#functions provide functionality
+#str()
+
+name= 'Antaeus'
+age = 39
+month = '2' 
+
+
+print(name)
+print(name + ' Birthday month is in', month + '!!')
+print(name + ' is ' + str(age) + ' years of age')
+
+
+
+
+
+
